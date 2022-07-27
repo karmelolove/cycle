5219be188f441e7db86f05812d31c098c75a98bd
karmelolove <zogofelixarmel15@gmail.com>
2022-07-27 12:20:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cycle/mestruelle.kt
@@ -0,0 +1,11 @@
+package com.example.cycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mestruelle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mestruelle)
+    }
+}
---
+++ app/src/main/java/com/example/cycle/suivie_grossesse.kt
@@ -0,0 +1,11 @@
+package com.example.cycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class suivie_grossesse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suivie_grossesse)
+    }
+}
